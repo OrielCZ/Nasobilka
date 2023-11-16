dc0d85b7629914488dbdab85347608c99759c4f1
OrielCZ <ondra.zach@gmail.com>
2023-11-16 09:43:05 +0100

    Dokonalý_výpis

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
-﻿int input = 15;
-int x = 20;
-for (int i = input; i <= x; i++) 
+﻿string inputstr;
+string input2str;
+string okolikstr;
+int input;
+int input2;
+int okolik;
+Console.WriteLine("První číslo: ");
+inputstr = Console.ReadLine();
+input = Convert.ToInt32(inputstr);
+
+Console.WriteLine("Druhé číslo: ");
+input2str = Console.ReadLine();
+input2 = Convert.ToInt32(input2str);
+
+Console.WriteLine("Okolik ");
+okolikstr = Console.ReadLine();
+okolik = Convert.ToInt32(okolikstr);
+
+for (int i = input; i <= input2; i += okolik ) 
 {
-  Console.WriteLine(i);
+  Console.Write(i + ", " );
 }
