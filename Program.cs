32a9678aaba7447993de51e1971c3dcbe331d8d6
OrielCZ <ondra.zach@gmail.com>
2023-11-16 09:12:42 +0100

    Číselná_řada

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-bool xdddd = true;
+﻿int input = 15;
+int x = 20;
+for (int i = input; i <= x; i++) 
+{
+  Console.WriteLine(i);
+}
